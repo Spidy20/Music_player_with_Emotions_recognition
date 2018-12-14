77ca0cb3b661bec28e059e15fae5631dd06ecc2e
Spidy20 <kushalbhavsar58@gmail.com>
2018-12-14 13:12:12 +0530

    New commit

--- music_player_android.py
+++ music_player_android.py
@@ -1,38 +1,60 @@
-#Coded by:- Kushal Bhavsra
-#From:- Techmicra IT solution
-import time
 import cv2
 import label_image
-import os,random
-import subprocess
+import os,random,subprocess
+import  numpy as np
+from urllib.request import  urlopen
+import time
+from playsound import playsound
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 size = 4
+
 # We load the xml file
 classifier = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
-global text
-webcam = cv2.VideoCapture(0)  # Using default WebCam connected to the PC.
-now = time.time()###For calculate seconds of video
-future = now + 20  ####here is second of time which taken by emotion recognition system ,you can change it
+mobile_video="http://192.168.0.109:8080/shot.jpg"
+now = time.time()
+future = now + 15
+  # Using default WebCam connected to the PC.
+
 while True:
-    (rval, im) = webcam.read()
-    im = cv2.flip(im, 1, 0)  # Flip to act as a mirror
-    # Resize the image to speed up detection
+
+    img_resp = urlopen(mobile_video)
+    img_arr = np.array(bytearray(img_resp.read()), dtype=np.uint8)
+
+    cap = cv2.imdecode(img_arr, -1)
+    im=cv2.flip(cap,1,0)
     mini = cv2.resize(im, (int(im.shape[1] / size), int(im.shape[0] / size)))
+
     # detect MultiScale / faces
     faces = classifier.detectMultiScale(mini)
+    global text
+
     # Draw rectangles around each face
     for f in faces:
         (x, y, w, h) = [v * size for v in f]  # Scale the shapesize backup
-        cv2.rectangle(im, (x, y), (x + w, y + h), (255, 255 ,25), 7)
-        # Save just the rectangle faces in SubRecFaces
         sub_face = im[y:y + h, x:x + w]
         FaceFileName = "test.jpg"  # Saving the current image from the webcam for testing.
         cv2.imwrite(FaceFileName, sub_face)
         text = label_image.main(FaceFileName)  # Getting the Result from the label_image file, i.e., Classification Result.
         text = text.title()  # Title Case looks Stunning.
         font = cv2.FONT_HERSHEY_TRIPLEX
-        cv2.putText(im, text, (x + w, y), font, 1, (0, 25, 255), 2)
+
+        if text == 'Angry':
+            cv2.rectangle(im, (x, y), (x + w, y + h), (0, 25, 255), 7)
+            cv2.putText(im, text, (x + h, y), font, 1, (0, 25,255), 2)
+
+        if text == 'Smile':
+            cv2.rectangle(im, (x, y), (x + w, y + h), (0,260,0), 7)
+            cv2.putText(im, text, (x + h, y), font, 1, (0,260,0), 2)
+
+        if text == 'Fear':
+            cv2.rectangle(im, (x, y), (x + w, y + h), (0, 255, 255), 7)
+            cv2.putText(im, text, (x + h, y), font, 1, (0, 255, 255), 2)
+
+        if text == 'Sad':
+            cv2.rectangle(im, (x, y), (x + w, y + h), (0,191,255), 7)
+            cv2.putText(im, text, (x + h, y), font, 1, (0,191,255), 2)
+
     # Show the image/
     cv2.imshow('Music player with Emotion recognition', im)
     key = cv2.waitKey(30)& 0xff
--- music_player_webcam.py
+++ music_player_webcam.py
@@ -1,55 +1,56 @@
+#Coded by:- Kushal Bhavsra
+#From:- Techmicra IT solution
+import time
 import cv2
 import label_image
-import os,random,subprocess
-import  numpy as np
-from urllib.request import  urlopen
-import time
-from playsound import playsound
+import os,random
+import subprocess
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 size = 4
-
 # We load the xml file
 classifier = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
-mobile_video="http://192.168.0.101:8080/shot.jpg"
-now = time.time()
-future = now + 17
-  # Using default WebCam connected to the PC.
-
+global text
+webcam = cv2.VideoCapture(0)  # Using default WebCam connected to the PC.
+now = time.time()###For calculate seconds of video
+future = now + 60  ####here is second of time which taken by emotion recognition system ,you can change it
 while True:
-
-    img_resp = urlopen(mobile_video)
-    img_arr = np.array(bytearray(img_resp.read()), dtype=np.uint8)
-
-    cap = cv2.imdecode(img_arr, -1)
-    im=cv2.flip(cap,1,0)
+    (rval, im) = webcam.read()
+    im = cv2.flip(im, 1, 0)  # Flip to act as a mirror
+    # Resize the image to speed up detection
     mini = cv2.resize(im, (int(im.shape[1] / size), int(im.shape[0] / size)))
-
     # detect MultiScale / faces
     faces = classifier.detectMultiScale(mini)
-    global text
-
     # Draw rectangles around each face
     for f in faces:
         (x, y, w, h) = [v * size for v in f]  # Scale the shapesize backup
-        cv2.rectangle(im, (x, y), (x + w, y + h), (0, 255, 0), 4)
-
-        # Save just the rectangle faces in SubRecFaces
         sub_face = im[y:y + h, x:x + w]
-
         FaceFileName = "test.jpg"  # Saving the current image from the webcam for testing.
         cv2.imwrite(FaceFileName, sub_face)
-
         text = label_image.main(FaceFileName)  # Getting the Result from the label_image file, i.e., Classification Result.
         text = text.title()  # Title Case looks Stunning.
         font = cv2.FONT_HERSHEY_TRIPLEX
-        cv2.putText(im, text, (x + w, y), font, 1, (0, 0, 255), 2)
 
-    # Show the image
+        if text == 'Angry':
+            cv2.rectangle(im, (x, y), (x + w, y + h), (0, 25, 255), 7)
+            cv2.putText(im, text, (x + h, y), font, 1, (0, 25,255), 2)
+
+        if text == 'Smile':
+            cv2.rectangle(im, (x, y), (x + w, y + h), (0,260,0), 7)
+            cv2.putText(im, text, (x + h, y), font, 1, (0,260,0), 2)
+
+        if text == 'Fear':
+            cv2.rectangle(im, (x, y), (x + w, y + h), (0, 255, 255), 7)
+            cv2.putText(im, text, (x + h, y), font, 1, (0, 255, 255), 2)
+
+        if text == 'Sad':
+            cv2.rectangle(im, (x, y), (x + w, y + h), (0,191,255), 7)
+            cv2.putText(im, text, (x + h, y), font, 1, (0,191,255), 2)
+
+    # Show the image/
     cv2.imshow('Music player with Emotion recognition', im)
-    key = cv2.waitKey(30) & 0xff
-
-    if time.time() > future:  ##after 20second music will play
+    key = cv2.waitKey(30)& 0xff
+    if time.time() > future:##after 20second music will play
         try:
             cv2.destroyAllWindows()
             mp = 'C:/Program Files (x86)/Windows Media Player/wmplayer.exe'
